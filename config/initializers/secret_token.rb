6d3e17cfae05648594f648abf7edd6cb9a05d6d5
Michael Emaschow <memaschow@gmail.com>
2013-02-16 16:12:28 +0100

    erster commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easypeople::Application.config.secret_token = '66a20971a66662ae94e2f5c320a377f288551cb7809cb7a549473dc7efc6f00ec1365807406750c119cbf7b59197792712031450dd54338a35376d1fa589c090'
